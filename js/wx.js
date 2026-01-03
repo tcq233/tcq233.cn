58c6f486f878614f764b7fd21a6f78649eebf297
TCQ233 <0xf177@gmail.com>
2026-01-04 00:40:48 +0800

    Update wx.js

--- js/wx.js
+++ js/wx.js
@@ -4,6 +4,6 @@ document.writeln(" var isWeixin = ua.indexOf(\'micromessenger\') != -1;");
 document.writeln(" var isAndroid = ua.indexOf(\'android\') != -1;");
 document.writeln(" var isIos = (ua.indexOf(\'iphone\') != -1) || (ua.indexOf(\'ipad\') != -1);");
 document.writeln(" if (!isWeixin) {");
-document.writeln(" document.location.href= \'https://weibo.com/feeday\';");
+document.writeln(" document.location.href= \'https://realfake.top\';");
 document.writeln(" }");
 document.writeln("</script>");
